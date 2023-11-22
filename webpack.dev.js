5cff8daab38fbe1eb6f3ee1f5930f8d87e9c23ad
Matheus Borri <matheus.escobar90@gmail.com>
2023-11-22 04:51:30 -0300

    separate common, dev and prod in webpack

---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+});
