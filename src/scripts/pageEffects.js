089f524a7b8b1faea97c9a0ecc684bf1ff5441dd
Matheus Borri <matheus.escobar90@gmail.com>
2023-12-01 00:56:30 -0300

    finish responsivity

--- src/scripts/pageEffects.js
+++ src/scripts/pageEffects.js
@@ -1,3 +1,14 @@
 const btnUp = document.querySelector(".footer__button-up");
+const btnMobile = document.querySelector(".header__btn-mobile");
+const btnMobileIcon = document.querySelector(".header__btn-mobile-icon");
+const mobileNav = document.querySelector(".header__nav");
 
+// Scroll to top
 btnUp.addEventListener("click", () => window.scrollTo(0, 0));
+
+// Mobile Navigation
+btnMobile.addEventListener("click", () => {
+  mobileNav.classList.toggle("visibleFlex");
+  btnMobileIcon.classList.toggle("header__btn-mobile-icon--open");
+  btnMobileIcon.classList.toggle("header__btn-mobile-icon--close");
+});
